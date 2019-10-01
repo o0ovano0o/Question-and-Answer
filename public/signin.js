fcd5d490d699ca492b33fbe39a70285cc380e627
dominan <minhanh.th99@gmail.com>
2019-10-02 01:07:28 +0700

    signin.js public

--- public/signin.js
+++ public/signin.js
@@ -1,41 +1,49 @@
-document.getElementById("user_name").focus();
+document.getElementById("InputName").focus();
 
-document.getElementById("user_name").onkeyup=function (e){
-KeyUp(e,this,"user_nickname");
+document.getElementById("InputName").onkeyup = function(e) {
+  KeyUp(e, this, "InputNName");
 }
-document.getElementById("user_nickname").onkeyup=function (e){
-KeyUp(e,this,"user_email");
+document.getElementById("InputNName").onkeyup = function(e) {
+  KeyUp(e, this, "InputEmail");
 }
-document.getElementById("user_email").onkeyup=function (e){
-KeyUp(e,this,"user_password");
+document.getElementById("InputEmail").onkeyup = function(e) {
+  KeyUp(e, this, "InputPhoneNum");
 }
-document.getElementById("user_password").onkeyup=function (e){
-KeyUp(e,this,"user_repassword");
+document.getElementById("InputPhoneNum").onkeyup = function(e) {
+  KeyUp(e, this, "InputPassword");
 }
-document.getElementById("user_repassword").onkeyup=function (e){
-KeyUp(e,this,"license");
+document.getElementById("InputPassword").onkeyup = function(e) {
+  KeyUp(e, this, "InputRePassW");
 }
-document.getElementById("license").onkeyup=function (e){
-KeyUp(e,this,"signin_");
+document.getElementById("InputRePassW").onkeyup = function(e) {
+  KeyUp(e, this, "exampleRadios1");
 }
+document.getElementById("exampleRadios1").onkeyup = function(e) {
+  KeyUp(e, this, "submit");
+}
+
 function KeyUp(e, that, nextAction) {
-    if(window.event) { e = window.event;}
-    if(e.keyCode ==13) {
-        document.getElementById(nextAction).focus();
-    }
+  if (window.event) {
+    e = window.event;
+  }
+  if (e.keyCode == 13) {
+    document.getElementById(nextAction).focus();
+  }
 }
+
 function titleCase(str) {
-  str= str.toLowerCase().split(' ');
-  var convertToArray = "";
-  for(var i =0; i<str.length;i++){
-      if(str[i] != "") convertToArray += str[i]+" ";
- }
- convertToArray = convertToArray.toLowerCase().split(' ');
+  str = str.toLowerCase().split(' ');
+  var convertToArray = '';
+  for (var i = 0; i < str.length; i++) {
+    if (str[i] != '') convertToArray += str[i] + ' ';
+  }
+  convertToArray = convertToArray.toLowerCase().split(' ');
   var result = convertToArray.map(function(val) {
     return val.replace(val.charAt(0), val.charAt(0).toUpperCase());
   });
   return result.join(' ');
 }
+
 function titleCase2(str) {
   var convertToArray = str.split(' ');
   var result = convertToArray.map(function(val) {
@@ -44,65 +52,37 @@ function titleCase2(str) {
 
   return result.join('');
 }
-function SubmitClick(){
 
-  var check=true;
-  if(document.getElementById("user_name").value==""){
-      document.getElementById("user_name").style.borderColor = "red";
-      check=false;
+function Email_() {
+  var email = document.getElementById("InputEmail").value;
+  var aCong = email.indexOf('@');
+  var dauCham = email.lastIndexOf('.');
+  if (email == '') {
+    return false;
+  } else if ((aCong < 1) || (dauCham < aCong + 2) || (dauCham + 2 > email.length)) {
+    alert("Email khong chinh xác");
+    return false;
+  } else {
+    return true;
   }
-  if(document.getElementById("user_nickname").value==""){
-      document.getElementById("user_nickname").style.borderColor = "red";
-      check=false;
-  }
-  if(document.getElementById("user_password").value==""){
-      document.getElementById("user_password").style.borderColor = "red";
-      check=false;
-  }
-  if(document.getElementById("user_repassword").value==""){
-      document.getElementById("user_repassword").style.borderColor = "red";
-      check=false;
-  }
-  else if(document.getElementById("user_password").value!=document.getElementById("user_repassword").value){
-    document.getElementById("user_password").style.borderColor = "red";
-    document.getElementById("user_repassword").style.borderColor = "red";
-    document.getElementById("er_pass").style.visibility="visible";
-    check=false;
-  }
-  if(document.getElementById("user_email").value==""){
-      document.getElementById("user_email").style.borderColor = "red";
-      check=false;
-  }
-
-  if(check==false ){
-    alert("Đăng kí không thành công !");
-  }
-  else {
-    alert("Đăng kí thành công!")
-    document.getElementById("user_name").value="";
-    document.getElementById("user_nickname").value="";
-    document.getElementById("user_password").value="";
-    document.getElementById("user_repassword").value="";
-    document.getElementById("user_email").value="";
-  }
-
-}
-function col_uname(){
-  document.getElementById("user_name").style.borderColor="blue";
-}
-function col_unname(){
-  document.getElementById("user_nickname").style.borderColor="blue";
-}
-function col_email(){
-  document.getElementById("user_email").style.borderColor="blue";
-}
-function col_pass(){
-  document.getElementById("user_password").style.borderColor="blue";
-}
-function col_repass(){
-  document.getElementById("user_repassword").style.borderColor="blue";
 }
 
-function Buttonclick(){
-  document.getElementById("er_pass").style.visibility="hidden";
+function checkpass() {
+  var pass = document.getElementById("InputPassword").value;
+  var repass = document.getElementById("InputRePassW").value;
+  if (repass != pass) {
+    alert("Mật khẩu không khớp");
+  }
+}
+
+function checkPhone() {
+  var phone = document.getElementById("InputPhoneNum").value;
+  var phoneNum = /^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$/;
+  if (phone == '') return false;
+  if (phone.match(phoneNum)) {
+    return true;
+  } else {
+    alert("số điện thoại không hợp lệ");
+    return false;
+  }
 }
