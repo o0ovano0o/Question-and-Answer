0110978527e84392bfa7f7682090ed4abb8f1334
o0ovano0o <o0ovano0o@users.noreply.github.com>
2019-10-06 15:28:32 +0700

    commit

--- routes/main.js
+++ routes/main.js
@@ -5,9 +5,22 @@ var conn = db.getConnection();
 
 router.get('/', function (req, res) {
     if(req.session.user) 
-   { 	
-   	signinUser=req.session.user;
-	res.render('main', {signinUser});
+   { 
+   signinUser=req.session.user;	
+   var defer = q.defer();
+	var query = conn.query("SELECT * FROM surveys ", {username: signin.username}, function(err,results) {
+		if (err) {
+			defer.reject(err);
+		}
+		else {
+			defer.resolve(results);
+		}
+	});
+	var dt = defer.promise;
+	dt.then(function(surveys){
+		var user = users[0];
+		res.render('main',{signinUser:signinUser,surveys:surveys});
+	});
 	}else
 	res.render('login',{data: {error:  "Mời bạn đăng nhập!"}});
 });
