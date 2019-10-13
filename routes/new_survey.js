b6d16c93ac9b27f27281d61b5bf5fa881b0bccf2
dominan <minhanh.th99@gmail.com>
2019-10-13 20:08:27 +0700

    in phiếu hỏi đáp (chưa có đáp án)

--- routes/new_survey.js
+++ routes/new_survey.js
@@ -1,23 +1,22 @@
 var express = require('express')
 var router = express.Router();
 var db = require('../model/database');
-var conn = db.getConnection(); 
+var conn = db.getConnection();
 
 
 router.get('/', function (req, res) {
-	
+
 	 if(req.session.user){
-		var id = req.query.id;
-		var query = conn.query("SELECT * FROM survey WHERE ?",{survey_id:id}, (err, surveys) => {
-	    if(err) throw err;
-	    else{
-	    survey=surveys[0];
-	    res.render('new_survey', {session: req.session.user,survey:survey}); } 
-	});
-	
-	}
-	else
-	res.render('login',{data: {error:  "Mời bạn đăng nhập!"}});
+				var id = req.query.id;
+				var query = conn.query("SELECT * FROM survey join squest on survey.survey_id=squest.surveysq_id WHERE ? ",{survey_id:id}, (err, surveys) => {
+		    		if(err) throw err;
+		    		else{
+		    				survey=surveys[0];
+								squest=surveys;
+								s_ans=surveys;
+		    				res.render('new_survey', {session: req.session.user,survey:survey, squest:squest}); }
+				});
+		}else res.render('login',{data: {error:  "Mời bạn đăng nhập!"}});
 });
 
 module.exports = router;
