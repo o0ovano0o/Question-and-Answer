f6219e3ed62fd7aa6666c5e5f8a3490626184373
dominan <minhanh.th99@gmail.com>
2019-10-29 22:49:56 +0700

    chỉnh sửa/xóa

--- routes/delete.js
+++ routes/delete.js
@@ -2,14 +2,43 @@ var express = require('express')
 var router = express.Router();
 var db = require('../model/database');
 var conn = db.getConnection();
+var q = require('q');
 router.get('/', function(req, res) {
   if (req.session.user) {
     var ids = req.query.ids;
+    var id = req.query.id;
     var query = conn.query("DELETE FROM squest WHERE ? ", {squest_id: ids}, (err, num) => {
       if (err) {throw err;}
 			else {
         //  res.header("Location: ");
-        res.render('test', {session:req.session.user, squest});
+        var defer = q.defer();
+        var query = conn.query("SELECT * FROM survey left join squest on survey.survey_id=squest.surveysq_id WHERE ? ", {survey_id: id}, (err, num) => {
+          if (err) {defer.reject(err);}
+    			else {
+            survey = num[0];
+            squest = num;
+            defer.resolve(num);
+          }
+        });
+
+        var dt = defer.promise;
+        dt.then(function(num) {
+    	      var query = conn.query("UPDATE survey SET views=views+1 WHERE ?", {survey_id: id}, (err, squests) => {
+    		        if (err) throw err;
+    		        else {}
+    	      });
+    	      var querysq = conn.query("SELECT squest.*, s_ans.* FROM squest left join s_ans on squest.squest_id=s_ans.question_id where ?", {surveysq_id: id}, (err, squests) => {
+    		        if (err) throw err;
+    		        else {
+    		          s_ans = squests;
+    		          res.render('test', {
+    		            session: req.session.user,
+    		            s_ans: s_ans,
+                    survey: num[0],
+                    squest : num,
+    		          });}
+    	      });
+        });
       }
     });
   }
