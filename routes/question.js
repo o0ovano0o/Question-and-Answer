fc8ad59dcf7d883bb9e4e30c8425736bfc20c20a
traidat <truong5a8@gmail.com>
2019-11-04 21:02:45 +0700

    cho phep chinh sua tai khoan

--- routes/question.js
+++ routes/question.js
@@ -59,7 +59,7 @@ router.post('/', function (req, res) {
 			};
 			var query1 = conn.query("INSERT INTO cmt_ans SET ?", ans_cmt, function(err,results) {
 				if (err) {
-					throw(err);
+					throw(err); 
 				}
 				else {
 					var sql = "SELECT * FROM aquest join user on aquest.author = user.username  WHERE question_id =  ?;SELECT * FROM aquest  join section on section.section_id = aquest.sections_id join user on section.author = user.username WHERE question_id = ?;SELECT * FROM ans_quest join user on ans_quest.author = user.username  WHERE question_id =  ?;SELECT * FROM cmt_ans join user on cmt_ans.author = user.username  WHERE question_id =  ?"
--- routes/yourprofile.js
+++ routes/yourprofile.js
@@ -49,4 +49,26 @@ router.get('/', function (req, res) {
 		res.render('login',{data: {error:  "Mời bạn đăng nhập!"}});
 });
 
+router.post('/', function (req, res) {
+	var newname = req.body.newname;
+	var newpass = req.body.newpass;
+	var id = req.query.id;
+	console.log(newpass, newname, id);
+	var query1 = "UPDATE user SET name = ?, password = ? WHERE user.user_id = ?";
+	conn.query(query1,[newname, newpass, id], function(err, question) {	
+		if (err) {
+			throw(err);
+		}
+		else {
+			req.session.user.name = newname;
+			req.session.user.password = newpass;
+			var query = conn.query("SELECT * FROM survey Order by startdate DESC;SELECT * FROM section",  (err, surveys) => {
+			if(err) throw err;
+			else{
+				res.render('main',{session: req.session.user, surveys}); } 
+			});
+		}
+	});
+});
+
 module.exports = router;
