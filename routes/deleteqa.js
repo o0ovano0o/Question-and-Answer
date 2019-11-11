a8e4cbb726190904cf12976e0cd7e7e5c36b9161
o0ovano0o <o0ovano0o@users.noreply.github.com>
2019-11-11 22:04:18 +0700

    xong xóa - đóng - mở lại. chưa có sửa do đell có giao diện sửa

---
+++ routes/deleteqa.js
@@ -0,0 +1,24 @@
+var express = require('express')
+var router = express.Router();
+var db = require('../model/database');
+var conn = db.getConnection();
+var q = require('q');
+
+router.get('/', function (req, res) {
+  if(req.session.user) {
+     var id = req.query.id;
+      var query = conn.query("delete from section WHERE ?", {section_id: id}, (err, squests) => {
+            if (err) throw err;
+            else {
+             res.redirect('/home');
+            }
+        });
+  }
+
+  else{
+    res.render('login',{data: {error:  "Mời bạn đăng nhập!"}});
+  }
+});
+
+
+module.exports = router;
