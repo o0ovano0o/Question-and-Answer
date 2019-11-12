d0e8e8810eefa303b63299b37b06fc236cce0ebf
traidat <truong5a8@gmail.com>
2019-11-12 13:53:17 +0700

    ghim cmt

--- routes/controller.js
+++ routes/controller.js
@@ -97,6 +97,7 @@ router.use('/home', require(__dirname + '/main.js'));
 router.use('/signin', require(__dirname + '/signin.js'));
 router.use('/deletesection', require(__dirname + '/deletesection.js'));
 // router.use('/createsession', require(__dirname + '/new_session.js'));
+router.use('/ghim', require(__dirname + '/ghim.js'));
 router.use('/session', require(__dirname + '/session.js'));
 router.use('/new_session', require(__dirname + '/new_session.js'));
 router.use('/question', require(__dirname + '/question.js'));
