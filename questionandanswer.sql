d3383b5e9fb2bd9b1aaf54036ed0c38148aeb1f9
traidat <truong5a8@gmail.com>
2019-11-12 16:10:21 +0700

    update db

--- questionandanswer.sql
+++ questionandanswer.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Máy chủ: 127.0.0.1
--- Thời gian đã tạo: Th10 12, 2019 lúc 10:07 AM
+-- Thời gian đã tạo: Th10 12, 2019 lúc 10:08 AM
 -- Phiên bản máy phục vụ: 10.1.38-MariaDB
 -- Phiên bản PHP: 7.3.2
 
